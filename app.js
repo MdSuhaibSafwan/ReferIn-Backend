452a1189835bf80855a2a2609ca081667b2963b3
Asaduzzaman <158209281+Asaduzzaman04@users.noreply.github.com>
2025-10-30 20:57:16 +0600

    fix cors issue add credentials

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://www.referin.io"]
+    origin: ["http://localhost:3000","https://www.referin.io"],
+    credentials: true,
   })
 );
 
@@ -25,12 +26,12 @@ app.post(
   stripeController.stripeWebhook
 );
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.setHeader("Access-Control-Allow-Methods", "*");
+//   next();
+// });
 
 app.use(
   "/auth", AuthRoutes,
